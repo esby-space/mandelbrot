8fd43002910d27d4d691113b829e2374a77d3271
esby <seabass.branch@gmail.com>
2022-03-06 17:48:27 -0500

    idk how to use express :(

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 import express from 'express';
+import { readFile } from 'fs/promises';
 
 const app = express();
-app.use(express.static('public', {
-    setHeaders: function (res, path) {
-        res.set('Cross-Origin-Opener-Policy', 'same-origin');
-        res.set('Cross-Origin-Embedder-Policy', 'require-corp');
-    }
-}));
+app.use(
+    express.static('public', {
+        setHeaders: (response, path) => {
+            response.set('Cross-Origin-Opener-Policy', 'same-origin');
+            response.set('Cross-Origin-Embedder-Policy', 'require-corp');
+        },
+    })
+);
+
+app.get('/', async (request, response) => {
+    const home = await readFile('./public/index.html', 'utf-8').catch(
+        (error) => {
+            response.send(error);
+            response.status(400);
+        }
+    );
+
+    response.send(home);
+    response.status(200);
+    console.log('hello there!');
+});
 
 app.listen(3000, () => {
     console.log('App is available at http://localhost:3000');
